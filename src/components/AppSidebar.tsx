c427f34cb85153df917aa09e6976224f556e8740
soutomluiz <wtom.luiizz@gmail.com>
2025-01-26 14:45:00 -0500

    Update AppSidebar.tsx

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -16,11 +16,11 @@ export function AppSidebar({ activeTab, setActiveTab }: AppSidebarProps) {
   return (
     <Sidebar className="bg-background border-r border-border">
       <SidebarHeader className="p-0 border-b border-border">
-        <div className="flex items-center justify-center">
+        <div className="flex items-center justify-center h-24">
           <img
             src="/logo.svg"
             alt="Logo"
-            className="h-18 w-18 my-0"
+            className="h-16 w-auto"
           />
         </div>
       </SidebarHeader>
